5efc3c6195b71ab51ef708dc674138138fbc6e63
NicoleBorbe <nicole.borbe@gmail.com>
2022-08-18 12:39:20 +0200

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function intro3(name, stadt, alter) {
+    console.log("Hallo, mein Name ist " + name + ". Ich bin " + alter + " Jahre alt und komme aus " + stadt + ".");
+}
+let name = "Nicole";
+let alter = 45;
+let stadt = "Berlin";
+intro3(name, stadt, alter);
+
+// Warum wird name durchgestrichen?
